b8b5f52c435d59ced5ba11ddc15e784ab9d110c8
Matthew Dangerfield <matthew.dangerfield@aidahealth.com>
2017-09-02 11:01:22 -0500

    add in logic for meetup provider

--- src/providers/meetup/meetup.ts
+++ src/providers/meetup/meetup.ts
@@ -1,6 +1,9 @@
 import { Injectable } from "@angular/core";
 import { Http } from "@angular/http";
+import "rxjs/add/operator/do";
 import "rxjs/add/operator/map";
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
+import { Observable } from "rxjs/Observable";
 
 /*
   Generated class for the MeetupProvider provider.
@@ -10,6 +13,7 @@ import "rxjs/add/operator/map";
 */
 @Injectable()
 export class MeetupProvider {
+  public meetupRsvps$: BehaviorSubject<MeetupRSVP[]> = new BehaviorSubject(null);
   private config = {
     apiKey: "",
     eventId: ""
@@ -18,16 +22,39 @@ export class MeetupProvider {
   constructor(public http: Http) {
   }
 
-  public updateConfig(newConfig) {
+  public getConfig() {
+    return this.config;
+  }
+
+  public updateConfig(newConfig): Observable<MeetupRSVP[]> {
     this.config = newConfig;
     return this.fetchMeetupMembers();
   }
 
-  private fetchMeetupMembers() {
+  private fetchMeetupMembers(): Observable<MeetupRSVP[]> {
     const apiUrl = `api/rsvps?event_id=${this.config.eventId}&key=${this.config.apiKey}&rsvp=yes&sign=true`;
+    const alternateAvatarUrl = "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png";
 
-    this.http.get(apiUrl)
-        .map(res => res.json().results)
-        .subscribe(console.log);
+    return this.http.get(apiUrl)
+               .map(res => res.json()
+                              .results
+                              .map(result => (
+                                  {
+                                    id: result.member.member_id,
+                                    name: result.member.name,
+                                    included: true,
+                                    photoUrl: result.member_photo ? result.member_photo.photo_link : alternateAvatarUrl
+                                  } as MeetupRSVP
+                                )
+                              )
+               )
+               .do((rsvps: MeetupRSVP[]) => this.meetupRsvps$.next(rsvps));
   }
+}
+
+export interface MeetupRSVP {
+  id: number;
+  name: string;
+  included: boolean;
+  photoUrl: string;
 }

b25608bc18b41dcd00d9888b252665f514b47c9a
Matthew Dangerfield <matthew.dangerfield@aidahealth.com>
2017-09-02 11:30:56 -0500

    add in logic for potential winners

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { NavController } from "ionic-angular";
 import { MeetupProvider, MeetupRSVP } from "../../providers/meetup/meetup";
 
@@ -6,18 +6,38 @@ import { MeetupProvider, MeetupRSVP } from "../../providers/meetup/meetup";
   selector: "page-home",
   templateUrl: "home.html"
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit, OnDestroy {
   public meetupRsvps: MeetupRSVP[];
+  public potentialWinners: MeetupRSVP[];
+  public winners: MeetupRSVP[] = [];
 
   constructor(public navCtrl: NavController,
               private meetupProvider: MeetupProvider) {
   }
 
-  ngOnInit() {
-    this.meetupProvider.meetupRsvps$.subscribe((newRsvps: MeetupRSVP[]) => this.meetupRsvps = newRsvps);
+  public ngOnInit() {
+    this.meetupProvider.meetupRsvps$.subscribe((newRsvps: MeetupRSVP[]) => {
+      this.meetupRsvps = newRsvps;
+      this.winners = [];
+      this.potentialWinners = newRsvps;
+    });
   }
 
-  ngOnDestroy() {
+  public updatePotentialWinners() {
+    if (!this.potentialWinners) return;
+
+    this.potentialWinners = this.meetupRsvps.filter(potentialWinner => potentialWinner.included);
+  }
+
+  public selectWinner() {
+    if (this.potentialWinners && this.potentialWinners.length) {
+      const randomIndex = Math.floor(Math.random() * this.potentialWinners.length);
+      this.winners.push(this.potentialWinners[randomIndex]);
+      this.potentialWinners.splice(randomIndex, 1);
+    }
+  }
+
+  public ngOnDestroy() {
     this.meetupProvider.meetupRsvps$.unsubscribe();
   }
 
